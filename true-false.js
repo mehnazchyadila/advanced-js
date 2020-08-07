cfbbc471601c4da1c4c39c8988bd8b63b209d667
mehanazchy <you@example.comadilapucctgbd@gmail.com>
2020-08-08 01:47:13 +0600

    Truthy & falsy

---
+++ true-false.js
@@ -0,0 +1,38 @@
+//Falsy
+// 0 
+// ""
+// undefined
+// false
+// NaN
+// null
+// truthy
+// " "
+// 'false'
+// '0',[],
+
+// const age = 4;
+// if(age > 0){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// const age = 0;
+// if(age > 0){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+const age = 4;
+if(age){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+// if name == "" (condition is false) & if name = name == " " (condition is true because of space)
+// if let name == not set (undefined & condition is false).
