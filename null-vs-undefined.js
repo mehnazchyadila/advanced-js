14e6dbf871327f39d72642ef2106d4e1f211dd6a
mehanazchy <you@example.comadilapucctgbd@gmail.com>
2020-08-08 21:10:32 +0600

    null vs undefined is cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+//When we get Undefined
+// let pakhi ;
+// console.log(pakhi);
+
+// function add(num1 , num2){
+//     console.log(num1 + num2);
+// }
+
+// function add(num1 , num2){
+//     console.log(num2);
+
+// }
+// const result = add(13);
+
+// const premik = {name: "Abulla" , phone: '01823647489'};
+// console.log(premik.gf);
+
+// const fun  = undefined ;
+
+// let array = [2,3,4,5,6,7];
+// console.log(array[11]);
+
+// null is usually not exit its actually set explicitly by developer 
+
+
+
