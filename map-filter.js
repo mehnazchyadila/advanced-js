8cd8036361ddc522e53ba85b4d375a81ebe2df06
mehanazchy <you@example.comadilapucctgbd@gmail.com>
2020-08-09 02:49:09 +0600

    Object is cleared for interview

--- map-filter.js
+++ map-filter.js
@@ -6,4 +6,19 @@ for(let i = 0 ; i < numbers.length; i++){
     output.push(result);
 }
 
-console.log(output);
+numbers.map(function(numbers , index ,array){
+    console.log(numbers,index,array);
+});
+
+const result = numbers.map(function(element){
+    return element * element ;
+});
+
+const result = numbers.map(x => x * x);
+console.log(result);
+
+const bigger = numbers.filter( x => x > 5);
+console.groupCollapsed(bigger);
+
+const isThere = numbers.find( x => x > 2);
+console.log(isThere);
